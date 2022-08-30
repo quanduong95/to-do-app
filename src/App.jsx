470728eb2f2b80ef3b7805b15a29dfd937163883
quan-duong <quan.dn95@gmail.com>
2022-08-30 16:14:58 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+
+
+}
+
+export default function App() {
+  return (
+    <div className={style.bg}>
+      hello
+    </div>
+  )
+}
